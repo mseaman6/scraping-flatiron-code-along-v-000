9330dd20d5cc713c2f017d63cd78042b6473010c
Monica Seaman <monica.l.seaman@gmail.com>
2018-05-28 17:14:59 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -14,6 +14,8 @@ class Scraper
       course.title = post.css("h2").text
       course.schedule = post.css(".date").text
       course.description = post.css("p").text
+    end
+    binding.pry
   end
 
   doc.css(".post").first.css("h2").text
