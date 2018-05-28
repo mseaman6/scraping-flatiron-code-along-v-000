3b3cac2a504df844aef0514c7de23b54b4908304
Monica Seaman <monica.l.seaman@gmail.com>
2018-05-28 17:20:23 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,8 +8,14 @@ class Scraper
 
   def get_page
     doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+  end
 
-    doc.css(".post").each do |post|
+  def get_courses
+    self.get_page.css(".post")
+  end
+
+  def make_courses
+    self.get_courses.each do |post|
       course = Course.new
       course.title = post.css("h2").text
       course.schedule = post.css(".date").text
